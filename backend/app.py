07dc36b35e489ab1bbc70a6c77eafd2683a6bc14
Sukruthachintham <139362181+Sukruthachintham@users.noreply.github.com>
2025-05-16 15:08:52 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -73,7 +73,8 @@ def delete_expense(expense_id):
 
 import os
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # default to 5000 for local testing
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
